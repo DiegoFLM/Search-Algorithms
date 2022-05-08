286d41d5a4b05af0fb30a0d5680251a6e45dd150
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-05-07 22:43:42 -0500

    Some global variables created

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -9,47 +9,61 @@ using namespace std;
 
 
 
-void printMap (int side, int mapa [10][10]){
-    for (int a = 0; a < side; a ++){
-        for(int b = 0; b < side; b++){
+int const L = 10;
+
+//Map of the labyrinth without robots position 
+int map[L][L] ={{0, 0, 0, 0, 0, 5, 1, 1, 4, 0},
+                {0, 1, 1, 1, 1, 0, 1, 1, 1, 0},
+                {0, 0, 0, 6, 6, 0, 0, 0, 0, 0},
+                {1, 6, 1, 1, 1, 1, 0, 1, 1, 6},
+                {1, 6, 1, 1, 1, 1, 0, 1, 1, 6},
+                {1, 6, 1, 0, 0, 0, 0, 0, 0, 3},
+                {1, 6, 1, 0, 1, 1, 1, 1, 0, 1},
+                {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
+                {1, 0, 0, 0, 6, 6, 6, 0, 0, 5},
+                };
+
+
+
+void printMap (int mapa [L][L]);
+void move (int op, int pos[1][2]);
+
+int main() {
+   
+
+
+    printMap(map);
+}
+
+
+
+void printMap (int mapa [L][L]){
+    for (int a = 0; a < L; a ++){
+        for(int b = 0; b < L; b++){
             cout << mapa[a][b] << " ";
         }
     cout << endl;
     }
 }
 
+void move (int op, int pos[1][2]){
+    
+}
 
-int main() {
-    /*
+
+
+
+
+ /*
     int array1[5];
     int s = sizeof(array1)/sizeof(int);
     fill(array1, array1 + s, 0);
     cout << sizeof(array1)/sizeof(int) << endl << s << endl;
     */
-
-    const size_t L = 10;
-
-    //Map of the labyrinth:
-    int map[L][L] ={{0, 0, 0, 0, 0, 5, 1, 1, 4, 0},
-                    {0, 1, 1, 1, 1, 0, 1, 1, 1, 0},
-                    {0, 0, 2, 6, 6, 0, 0, 0, 0, 0},
-                    {1, 6, 1, 1, 1, 1, 0, 1, 1, 6},
-                    {1, 6, 1, 1, 1, 1, 0, 1, 1, 6},
-                    {1, 6, 1, 0, 0, 0, 0, 0, 0, 3},
-                    {1, 6, 1, 0, 1, 1, 1, 1, 0, 1},
-                    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 6, 6, 6, 0, 0, 5},
-                    };
-
+    //const size_t L = 10;
+    
     //int mapSize = sizeof(map)/sizeof(int[1]);
     //cout << mapSize << endl;
 
     //fill(*map, *map + L * L, 0);
-
-
-   
-
-    printMap(10, map);
-
-}
